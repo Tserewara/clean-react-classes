23fc5bef806121b4f24d383099fd6ed7b6de982f
Tserewara <alvaro.alf2@gmail.com>
2021-12-07 08:38:16 -0300

    refactor: change input to use prop-types

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Styles from './spinner-styles.scss'
+import PropTypes from 'prop-types'
 
 type Props = React.HTMLAttributes<HTMLElement>
 
-const Spinner: React.FC<Props> = (prop: Props) => {
+const Spinner: React.FC<Props> = (props: Props) => {
   return (
-    <div {...prop} className={[Styles.spinner, prop.className].join(' ')}>
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}>
       <div /><div /><div /><div />
     </div>
   )
+}
+
+Spinner.propTypes = {
+  className: PropTypes.string
 }
 
 export default Spinner
