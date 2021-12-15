f0ab020cc347a1bd94ba251c3d4fb30bb7af0046
Tserewara <alvaro.alf2@gmail.com>
2021-12-15 08:31:38 -0300

    test: ensure ValidationBuilder returns a list of validations

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,16 @@
-/* eslint-disable indent */
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-        '<rootDir>/src/**/*.{ts,tsx}',
-        '!**/*.d.ts'
-    ],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-    transform: {
-        '.+\\.(ts|tsx)$': 'ts-jest'
-    },
-    moduleNameMapper: {
-        '@/(.*)': '<rootDir>/src/$1',
-        '\\.scss$': 'identity-obj-proxy'
-    }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
 }
